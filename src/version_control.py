ce0f485149a71c4b6ba574f75335f63909e13f4e
Emgalves <emilia.mga@gmail.com>
2025-04-23 10:03:31 -0300

    Update version_control.py

--- src/version_control.py
+++ src/version_control.py
@@ -9,17 +9,19 @@ from pathlib import Path
 # Informações da versão atual
 VERSION_INFO = {
     "major": 1,
-    "minor": 1,
+    "minor": 2,
     "patch": 0,
-    "release_date": "14/03/2025",
+    "release_date": "24/04/2025",
     "changes": [
-        "Separação de despesas com funcionários, no Relatório, separando despesas mensais das eventuais",
-        "Correção do campo percentual e inclusão de dados bancários na Entrada em Gestão de Contrato",
-        "Melhoria do layout da tela de Seleção de Cliente",
-        "Correção da combobox Banco em Fornecedor",
-        "Reorganização da aba Fornecedor com funcionalidade de duplo clique",
-        "Ajuste do layout da aba Entrada de Dados para melhor usabilidade",
-        "Adição de preenchimento automático da referência baseado na especificação do fornecedor"
+        "Criação do módulo para rateio de despesas comuns a todos os clientes",
+        "Criação do módulo de controle de medições de empreiteiros",
+        "Ajuste na aba Clientes, permitindo a inclusão do témino da obra ou do contrato",
+        "Criação de uma interface com todos os relatórios",
+        "Criação de relatório de Fornecedores",
+        "Criação de relatório de Medições",
+        "Criação de relatório de Tipos de Despesas"
+        "Separação de relatório de Lançamentos Futuros"
+        "Inclusão das etapas de obra nos contratos de administração de obras"
     ]
 }
 
