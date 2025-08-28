9b90d783aca1675eee10eb3942ecd8b878b60b24
Emgalves <emilia.mga@gmail.com>
2025-08-28 13:02:58 -0300

    Update version_control.py

--- src/version_control.py
+++ src/version_control.py
@@ -10,18 +10,11 @@ from pathlib import Path
 VERSION_INFO = {
     "major": 1,
     "minor": 4,
-    "patch": 0,
-    "release_date": "22/08/2025",
+    "patch": 1,
+    "release_date": "28/08/2025",
     "changes": [
-        "Adição de funcionalidade para recálculo de taxa de administração por percentual, sempre que houver inclusão, alteração ou exclusão de lançamento",
-        "Correção do cálculo de taxa de administração, considerando apenas lançamentos do tipo 'ATIVO'",
-        "Melhoria na funcionalidade que detecta duplicidade de lançamento",
-        "Inclusão do campo 'NF' no Gerenciador de Lançamentos",
-        "Inclusão de botões de 'EXCLUSÃO/RESTAURAÇÃO' de várias despesas no Gerenciador de Lançamentos",
-        "Inclusão do campo 'ETAPA DA OBRA' na Entrada de Dados, com manutenção através das Configurações do Sistema",
-        "Inclusão de gerenciamento para excluir CPF temporário",
-        "Inclusão de campo 'INSUMO' na Entrada de Dados, com manutenção através das Configurações do Sistema",
-        "Inclusão de funcionalidade para importaçãoo de NFe via XML e Chave de Acesso, permitindo salvar dados financeiros e materiais na planilha do cliente"
+        "Melhoria no Visualizador de Lançamentos de Fornecedor que agora possui todas as funcionalidades do Gerenciador de Lançamentos",
+        "Correção na aba Entrada de Dados que não estava limpando corretamente todos os campos"
     ]
 }
 
