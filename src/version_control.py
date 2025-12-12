7c2df388ad65961ba7fb1d2551825de709ddee52
Emgalves <emilia.mga@gmail.com>
2025-12-12 18:42:30 -0300

    Update version_control.py

--- src/version_control.py
+++ src/version_control.py
@@ -11,7 +11,7 @@ VERSION_INFO = {
     "major": 1,
     "minor": 5,
     "patch": 2,
-    "release_date": "11/12/2025",
+    "release_date": "12/12/2025",
     "changes": [
         "Incluída a opção de vincular várias medições a um único pagamento",
         "Criação do relatório gerencial de contratos de empreiteiros",
@@ -20,6 +20,7 @@ VERSION_INFO = {
         "Incluída a opção de copiar as fases/eventos de um gestor para outro no contrato de administração de obra",
         "Incluída a opção de vincular várias parcelas de contrato de administração de obra a um lançamento na planilha do cliente",
         "Incluída a opção de contrato de medição apenas com prazo, sem data início e fim",
+        "Incluída a opção de importar medições originárias do Relatório de Medições",
         "Inclusão e gestão de medições pendentes na Agenda"
     ]
 }
